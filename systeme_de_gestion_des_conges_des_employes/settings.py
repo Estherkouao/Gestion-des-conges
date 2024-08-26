93d73d9f70211700abf08b0a7b96805a617df76a
kouao amah esther <estherkouao9@gmail.com>
2024-08-26 11:06:35 +0000

    nouvaux2

--- systeme_de_gestion_des_conges_des_employes/settings.py
+++ systeme_de_gestion_des_conges_des_employes/settings.py
@@ -11,24 +11,28 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ 
 
 import os
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(env_file=str(BASE_DIR / "systeme_de_gestion_des_conges_des_employes" / ".env") )
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c62(q#)8*##y%@+nx1=ut@9b511v2#01w47*z#%858%u3b__%!'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 # Application definition
